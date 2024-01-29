4bc507f8beb05f57524cb41a83f510197c557ba0
marlon tunes <smarlon6@gmail.com>
2024-01-29 00:58:05 -0400

    Configura Menssagens de Alerta no Django(Boostrap)

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'messagem': messages.success(request, 'Esta é uma mensagem de sucesso!')
+    }
+    return render(request, 'index.html', context)
